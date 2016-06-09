fc132fc81e198ec4f6f8ce952d3a254ac1c7a477
Peter <mitranopeter@gmail.com>
2016-06-09 18:44:20 -0400

    changed hardcoded path
    this should really be fixed properly

--- vector_common/vector_config/env-hooks/50.vector_config.sh
+++ vector_common/vector_config/env-hooks/50.vector_config.sh
@@ -1,4 +1,4 @@
 # This configures the environment variables for a vector robot in simulation and real hardware.
 # This is necessary to run before starting the simulation or the platform.
 
-source $CATKIN_ENV_HOOK_WORKSPACE/../src/vector_common/vector_config/vector_config.sh
+source $CATKIN_ENV_HOOK_WORKSPACE/../src/vector_v1/vector_common/vector_config/vector_config.sh
