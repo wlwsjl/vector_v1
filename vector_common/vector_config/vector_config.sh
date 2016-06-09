cb5a376ed166b755b0b55961860c6b92ad8a53af
Peter <mitranopeter@gmail.com>
2016-06-09 16:10:49 -0400

    changed serial numbers for jaco arms
    this change was probably made by sid but I'm going to assume we need it

--- vector_common/vector_config/vector_config.sh
+++ vector_common/vector_config/vector_config.sh
@@ -92,8 +92,8 @@ export FLEA3_RPY="0 0 0"
 #Kinova arm configurations (the right arm should be the default if there is only one)
 export VECTOR_HAS_KINOVA_ARM=true
 export VECTOR_HAS_TWO_KINOVA_ARMS=false
-export KINOVA_RIGHT_ARM_SERIAL_NUMBER='PJ00900006153340003' #default arm if there is only one
-export KINOVA_LEFT_ARM_SERIAL_NUMBER='PJ00900006152990001' #must specify serial number for 2 arms
+export KINOVA_RIGHT_ARM_SERIAL_NUMBER='PJ00900006153340001' #default arm if there is only one
+export KINOVA_LEFT_ARM_SERIAL_NUMBER='' #must specify serial number for 2 arms
 
 #Robotiq gripper configurations
 export VECTOR_HAS_ROBOTIQ_GRIPPER=true
