2cea9b4b1f2f59ab4d97abf8b3c9f9c6ab93d514
PatrickHussey <dev@stanleyinnovation.com>
2016-06-24 14:17:12 -0400

    fixing so the path is not relative

--- vector_common/vector_config/env-hooks/50.vector_config.sh
+++ vector_common/vector_config/env-hooks/50.vector_config.sh
@@ -1,4 +1,4 @@
 # This configures the environment variables for a vector robot in simulation and real hardware.
 # This is necessary to run before starting the simulation or the platform.
 
-source $CATKIN_ENV_HOOK_WORKSPACE/../src/vector_common/vector_config/vector_config.sh
+source $(rospack find vector_config)/vector_config.sh
