14dc74526555ad892543fa78828b16a870b109c3
Ajinkya Jain <jainajinkya92@gmail.com>
2017-04-03 12:27:02 -0500

    Removing teb local planner. No longer needed

--- vector_network/vector_network_config.bash
+++ vector_network/vector_network_config.bash
@@ -6,10 +6,10 @@
 #export ROBOT_NETWORK=eth0
 
 #Uncomment if vector1
-export ROBOT_NETWORK=br0
+#export ROBOT_NETWORK=br0
 
 #Uncomment if simulation system
-#export ROBOT_NETWORK=wlan0
+export ROBOT_NETWORK=wlan0
 
 #ROS IP needs to be set no matter what PC
 export ROS_IP=$(ip -4 address show $ROBOT_NETWORK | grep 'inet' | sed 's/.*inet \([0-9\.]\+\).*/\1/')
@@ -19,7 +19,7 @@ export ROS_IP=$(ip -4 address show $ROBOT_NETWORK | grep 'inet' | sed 's/.*inet 
 #export ROS_MASTER_URI=http://$ROS_MASTER_IPADD:11311/
 
 #Uncomment if vector1
-export ROS_MASTER_URI=http://$ROS_IP:11311/
+#export ROS_MASTER_URI=http://$ROS_IP:11311/
 
 #Uncomment if simulation system
-#export ROS_MATER_URI=http://vector1:11311/
+export ROS_MATER_URI=http://vector1:11311/
