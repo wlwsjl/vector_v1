0474e34fb9d6e2b69d4858fbdecb67cd5fb11a40
Ajinkya Jain <jainajinkya92@gmail.com>
2017-05-25 21:03:51 -0500

    converted the Jaco wrist joint to a revoulte joint instead of a continuous joint. Screwing motion enabled

--- vector_network/vector_network_config.bash
+++ vector_network/vector_network_config.bash
@@ -9,10 +9,12 @@
 #export ROBOT_NETWORK=br0
 
 #Uncomment if simulation system
-export ROBOT_NETWORK=wlan0
+#export ROBOT_NETWORK=wlan0
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://localhost:11311
 
 #ROS IP needs to be set no matter what PC
-export ROS_IP=$(ip -4 address show $ROBOT_NETWORK | grep 'inet' | sed 's/.*inet \([0-9\.]\+\).*/\1/')
+#export ROS_IP=$(ip -4 address show $ROBOT_NETWORK | grep 'inet' | sed 's/.*inet \([0-9\.]\+\).*/\1/')
 
 # Uncomment if vector2
 #export ROS_MASTER_IPADD=$(gethostip vector1 | awk '{print $2}')
@@ -22,4 +24,4 @@ export ROS_IP=$(ip -4 address show $ROBOT_NETWORK | grep 'inet' | sed 's/.*inet 
 #export ROS_MASTER_URI=http://$ROS_IP:11311/
 
 #Uncomment if simulation system
-export ROS_MATER_URI=http://vector1:11311/
+#export ROS_MATER_URI=http://vector1:11311/
